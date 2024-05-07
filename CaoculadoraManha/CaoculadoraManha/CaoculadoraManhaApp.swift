d2871fd7631ac427713a2800ec177a7712322c50
ldcss <lucass.dcss75@gmail.com>
2024-05-07 11:39:19 -0300

    <feat> add new calculadora project

---
+++ CaoculadoraManha/CaoculadoraManha/CaoculadoraManhaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaoculadoraManhaApp.swift
+//  CaoculadoraManha
+//
+//  Created by Lucas Daniel Costa da Silva on 07/05/24.
+//
+
+import SwiftUI
+
+@main
+struct CaoculadoraManhaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
