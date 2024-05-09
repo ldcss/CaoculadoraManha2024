c8829117a1c95891cee809e4448e0e6e89528423
ldcss <lucass.dcss75@gmail.com>
2024-05-09 11:53:38 -0300

    <feat> add new image and button

--- CaoculadoraManha/CaoculadoraManha/ContentView.swift
+++ CaoculadoraManha/CaoculadoraManha/ContentView.swift
@@ -11,14 +11,44 @@ struct ContentView: View {
   
   @State var years: Int?
   @State var months: Int?
+  @State var result: Int?
   
   var body: some View {
     VStack(alignment: .leading) {
       Text("Qual a idade do seu cão?")
+      
       Text("Anos")
+      
       TextField("Quantos anos completos tem seu cão", value: $years, format: .number)
+      
       Text("Meses")
-      TextField("Quantos meses completos tem seu cão", value: $months, format: .number)
+      
+      TextField("Quantos meses além disso ele tem", value: $months, format: .number)
+      
+      Text("Porte")
+      //segmented control
+      
+      if let result {
+        Text("Seu cachorro tem, em idade humana...")
+        Text("\(result) anos")
+      } else {
+        Image(ImageResource.clarinha)
+          .resizable()
+          .scaledToFit()
+          .frame(maxHeight: 150)
+  //      UIScreen.main.bounds.width
+        .frame(maxWidth: .infinity)
+      }
+      
+      Button("Cãocular"){
+        result = 23
+      }
+      .frame(maxWidth: .infinity)
+      .frame(height: 50)
+      .background(.indigo)
+      .foregroundStyle(.white)
+      .clipShape(.rect(cornerRadius: 10))
+      .bold()
     }
     .textFieldStyle(.roundedBorder)
     .keyboardType(.numberPad)
