592db7277007eaf44dacbac3c3e31b4795b7b8e4
ldcss <lucass.dcss75@gmail.com>
2024-05-10 11:53:09 -0300

    <feat> add spacer, divider and logic to calculate dog age

--- CaoculadoraManha/CaoculadoraManha/ContentView.swift
+++ CaoculadoraManha/CaoculadoraManha/ContentView.swift
@@ -8,25 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
-  
   @State var years: Int?
   @State var months: Int?
   @State var result: Int?
   
+  let portes = ["Pequeno", "Médio", "Grande"]
+  @State var porteSelecionado = "Pequeno"
+  
   var body: some View {
-    VStack(alignment: .leading) {
+    
+    VStack(alignment: .leading, spacing: 20) {
+      git commit -m "<feat> add spacer, divider and logic to calculate dog age"
       Text("Qual a idade do seu cão?")
-      
       Text("Anos")
-      
       TextField("Quantos anos completos tem seu cão", value: $years, format: .number)
-      
       Text("Meses")
-      
       TextField("Quantos meses além disso ele tem", value: $months, format: .number)
-      
       Text("Porte")
-      //segmented control
+      
+      Picker("Portes", selection: $porteSelecionado) {
+        ForEach(portes, id: \.self){ porte in
+          Text(porte)
+        }
+      }.pickerStyle(.segmented)
+      
+      Divider()
+      
+      Spacer()
       
       if let result {
         Text("Seu cachorro tem, em idade humana...")
@@ -36,23 +44,50 @@ struct ContentView: View {
           .resizable()
           .scaledToFit()
           .frame(maxHeight: 150)
-  //      UIScreen.main.bounds.width
-        .frame(maxWidth: .infinity)
+        //      UIScreen.main.bounds.width
+          .frame(maxWidth: .infinity)
+          .shadow(radius: 10)
       }
       
-      Button("Cãocular"){
-        result = 23
-      }
-      .frame(maxWidth: .infinity)
-      .frame(height: 50)
-      .background(.indigo)
-      .foregroundStyle(.white)
-      .clipShape(.rect(cornerRadius: 10))
-      .bold()
+      Spacer()
+      
+      Button("Cãocular", action: processYears)
+        .frame(maxWidth: .infinity)
+        .frame(height: 50)
+        .background(.indigo)
+        .foregroundStyle(.white)
+        .clipShape(.rect(cornerRadius: 10))
+        .bold()
     }
     .textFieldStyle(.roundedBorder)
     .keyboardType(.numberPad)
     .padding()
+    
+  }
+  
+  func processYears(){
+    print("cãocular")
+    guard let years, let months else {
+      print("campos não preenchidos")
+      return
+    }
+    guard months > 0 || years > 0 else {
+      print("pelo menos um campo deve ser maior que zero")
+      return
+    }
+    
+    let multiplicador: Int
+    switch porteSelecionado {
+    case "Pequeno":
+      multiplicador = 6
+    case "Médio":
+      multiplicador = 7
+    case "Grande":
+      multiplicador = 8
+    default:
+      multiplicador = 0
+    }
+    result = (years * multiplicador) + (multiplicador * (months / 12))
   }
 }
 
