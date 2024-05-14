2987c0628a189ba70b9a6b50de0efaf2844f95eb
ldcss <lucass.dcss75@gmail.com>
2024-05-14 11:02:44 -0300

    <feat> create navigation bar and scroll view

--- CaoculadoraManha/CaoculadoraManha/ContentView.swift
+++ CaoculadoraManha/CaoculadoraManha/ContentView.swift
@@ -16,67 +16,81 @@ struct ContentView: View {
   
   var body: some View {
     
-    VStack(alignment: .leading, spacing: 20) {
-      
-      Text("Qual a idade do seu cão?")
-        .font(.header5)
-      Text("Anos")
-        .font(.body1)
-      TextField("Quantos anos completos tem seu cão", value: $years, format: .number)
-      Text("Meses").font(.body1)
-      TextField("Quantos meses além disso ele tem", value: $months, format: .number)
-      Text("Porte")
-        .font(.body1)
-      
-      Picker("Portes", selection: $porteSelected) {
-        ForEach(Porte.allCases, id: \.self){ porte in
-          Text(porte.rawValue)
+    NavigationStack {
+      ScrollView {
+        VStack(alignment: .leading, spacing: 20) {
+          
+          Text("Qual a idade do seu cão?")
+            .font(.header5)
+          Text("Anos")
+            .font(.body1)
+          TextField("Quantos anos completos tem seu cão", value: $years, format: .number)
+          Text("Meses").font(.body1)
+          TextField("Quantos meses além disso ele tem", value: $months, format: .number)
+          Text("Porte")
+            .font(.body1)
+          
+          Picker("Portes", selection: $porteSelected) {
+            ForEach(Porte.allCases, id: \.self){ porte in
+              Text(porte.rawValue)
+            }
+          }.pickerStyle(.segmented)
+          
+          Divider()
+          
+          Spacer()
+          
+          if let result {
+            Text("Seu cachorro tem, em idade humana...")
+              .font(.body1)
+            Text("\(result) anos")
+              .font(.display)
+          } else {
+            Image(ImageResource.clarinha)
+              .resizable()
+              .scaledToFit()
+              .frame(maxHeight: 150)
+            //      UIScreen.main.bounds.width
+              .frame(maxWidth: .infinity)
+              .shadow(radius: 10)
+          }
+          
+          Spacer()
+          
+          Button("Cãocular", action: processYears)
+            .frame(maxWidth: .infinity)
+            .frame(height: 50)
+            .background(.indigo)
+            .foregroundStyle(.white)
+            .clipShape(.rect(cornerRadius: 10))
+            .font(.body1)
         }
-      }.pickerStyle(.segmented)
-      
-      Divider()
-      
-      Spacer()
-      
-      if let result {
-        Text("Seu cachorro tem, em idade humana...")
-          .font(.body1)
-        Text("\(result) anos")
-          .font(.display)
-      } else {
-        Image(ImageResource.clarinha)
-          .resizable()
-          .scaledToFit()
-          .frame(maxHeight: 150)
-        //      UIScreen.main.bounds.width
-          .frame(maxWidth: .infinity)
-          .shadow(radius: 10)
+        .textFieldStyle(.roundedBorder)
+        .keyboardType(.numberPad)
+        .padding()
+        .containerRelativeFrame(.vertical)
       }
-      
-      Spacer()
-      
-      Button("Cãocular", action: processYears)
-        .frame(maxWidth: .infinity)
-        .frame(height: 50)
-        .background(.indigo)
-        .foregroundStyle(.white)
-        .clipShape(.rect(cornerRadius: 10))
-        .font(.body1)
+      .navigationTitle("Cãoculadora")
+      .scrollDismissesKeyboard(.immediately)
+      .toolbarBackground(.indigo, for: .navigationBar)
+      .toolbarBackground(.visible, for: .navigationBar)
+      .toolbarColorScheme(.dark, for: .navigationBar)
     }
-    .textFieldStyle(.roundedBorder)
-    .keyboardType(.numberPad)
-    .padding()
-    
+    .fontDesign(.rounded)
   }
   
   func processYears(){
     print("cãocular")
     guard let years, let months else {
-      print("campos não preenchidos")
+      print(
+        "campos não preenchidos"
+      )
       return
     }
     guard months > 0 || years > 0 else {
-      print("pelo menos um campo deve ser maior que zero")
+      print(
+        "pelo menos um campo deve ser maior que zero"
+      )
       return
     }
     
